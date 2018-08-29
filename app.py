5913aeb1da58841940b67015f1981a5399bd5712
hl <huanglei0814@foxmail.com>
2018-08-30 00:31:10 +0800

    aaaaaaaaaaaa

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_restful import abort, reqparse, Resource, Api
+from flask_restful.representations import json
+from peewee import *
+from playhouse.shortcuts import model_to_dict
 
 app = Flask(__name__)
 api = Api(app)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 FOODS = {
     'foodid1': {'name': 'food 1', 'price': 24},
@@ -14,50 +19,84 @@ FOODS = {
     'foodid3': {'name': 'food 3', 'price': 26},
 }
 
+
 def abort_if_food_doesnt_exist(food_id):
     if food_id not in FOODS:
-        abort(404,message="food {} doesn't exist".format(food_id))
+        abort(404, message="food {} doesn't exist".format(food_id))
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('name')
 parser.add_argument('price')
 
-#Food
-#shows a single food item and lets you delete a food item
+db = SqliteDatabase('food.db')
+
+
+class Dish(Model):
+    id = PrimaryKeyField()
+    name = CharField()
+    price = DoubleField()
+
+    class Meta:
+        database = db  # This model uses the "food.db" database.
+
+
+# Food
+# shows a single food item and lets you delete a food item
 class Food(Resource):
-    def get(self,food_id):
-        abort_if_food_doesnt_exist(food_id)
-        return FOODS[food_id]
+    def get(self, food_id):
+        return model_to_dict(Dish.get(Dish.id == food_id))
 
-    def delete(self,food_id):
-        abort_if_food_doesnt_exist(food_id)
-        del FOODS[food_id]
-        return  '',204
+    def delete(self, food_id):
+        dish = Dish.get(Dish.id == food_id)
+        return dish.delete_instance(), 204
 
-    def put(self,food_id):
+    def put(self, food_id):
         args = parser.parse_args()
-        task = {'name':args['name'],'price':args['price']}
-        FOODS[food_id] = task
-        return task, 201
+        dish = Dish.get(Dish.id == food_id)
+        keys = args.keys()
+        for key in keys:
+            if args[key]!= None:
+                dish.__dict__['__data__'][key] = args[key]
+        return dish.save(), 201
 
-#FoodList
+
+# FoodList
 # shows a list of all foods, and lets you POST to add new foods
 class FoodList(Resource):
     def get(self):
-        return FOODS
+        foods = Dish.select().dicts()
+        fs = []
+        for row in foods:
+            fs.append(row)
+        return fs
+
 
     def post(self):
         args = parser.parse_args()
-        food_id = int(max(FOODS.keys()).lstrip('foodid')) + 1
-        food_id = 'foodid%i' % food_id
-        FOODS[food_id] = {'name':args['name'],'price':args['price']}
-        return FOODS[food_id], 201
+        dish = Dish()
+        keys = args.keys()
+        if 'name' in keys:
+            dish.name = args['name']
+        if 'price' in keys:
+            dish.price = args['price']
+        return dish.save(), 201
+
 
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(FoodList, '/foods')
 api.add_resource(Food, '/foods/<food_id>')
+
+
+def initialize_db():
+    db.connect()
+    db.create_tables([Dish], safe=True)
+    db.close()
+
+
+initialize_db()
 
 if __name__ == '__main__':
     app.run()
